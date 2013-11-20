923add287f9717ce216b5ed0785c1f7016e7f1f6
Simon Fell <fellforce@gmail.com>
2013-11-20 10:54:07 -0800

    write © / license comment at top of header

--- WSDL2ZKSforce.scala
+++ WSDL2ZKSforce.scala
@@ -30,6 +30,8 @@ class ComplexTypeInfo(xmlName: String, fields: Seq[ComplexTypeProperty]) extends
 		val hfile = new File(new File("output"), objcName + ".h")
 		hfile.getParentFile().mkdirs()
 		val h = new PrintWriter(hfile)
+		val dir = direction.result()
+		writeComment(h);
 		h.println(("""#import "zkDeserializer.h"
 					|@interface """ + objcName + """ : ZKXMLDeserializer {
 					|}
@@ -38,6 +40,29 @@ class ComplexTypeInfo(xmlName: String, fields: Seq[ComplexTypeProperty]) extends
 			h.println(f.readonlyPropertyDecl)
 		h.println("@end")
 		h.close()
+	}
+	
+	private def writeComment(w: PrintWriter) {
+		w.println("""/// Copyright (c) 2006-2013 Simon Fell
+		///
+		/// Permission is hereby granted, free of charge, to any person obtaining a 
+		/// copy of this software and associated documentation files (the "Software"), 
+		/// to deal in the Software without restriction, including without limitation
+		/// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+		/// and/or sell copies of the Software, and to permit persons to whom the 
+		/// Software is furnished to do so, subject to the following conditions:
+		///
+		/// The above copyright notice and this permission notice shall be included 
+		/// in all copies or substantial portions of the Software.
+		///
+		/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+		/// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+		/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+		/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+		/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+		/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+		/// THE SOFTWARE.
+		///""".stripMargin('/'));
 	}
 }
 
