730af23d77280ddde5b7868a1e2e348e9b7d002e
Simon Fell <fellforce@gmail.com>
2013-11-20 10:48:43 -0800

    track if the complexType requires serialization and/or deserialization support

--- WSDL2ZKSforce.scala
+++ WSDL2ZKSforce.scala
@@ -18,8 +18,14 @@ class ComplexTypeProperty(val name: String, val propType: TypeInfo) {
 	}
 }
 
+object Direction extends Enumeration {
+	val Serialize, Deserialize = Value
+}
+
 class ComplexTypeInfo(xmlName: String, fields: Seq[ComplexTypeProperty]) extends TypeInfo(xmlName, "ZK" + xmlName.capitalize, "retain") {
 	
+	val direction =  Direction.ValueSet.newBuilder
+		
 	def writeHeaderFile() {
 		val hfile = new File(new File("output"), objcName + ".h")
 		hfile.getParentFile().mkdirs()
@@ -35,6 +41,7 @@ class ComplexTypeInfo(xmlName: String, fields: Seq[ComplexTypeProperty]) extends
 	}
 }
 
+
 class Schema(wsdl: Elem, typeMapping: Map[String, TypeInfo]) {
 	private val complexTypeElems = createComplexTypesElems(wsdl)
 	private val complexTypes = collection.mutable.Map[String, ComplexTypeInfo]()
@@ -43,14 +50,24 @@ class Schema(wsdl: Elem, typeMapping: Map[String, TypeInfo]) {
 	
 	val messages = createMessages(wsdl)
 
-	def handleElement(elementName: String) {
-		// walk through the element decl and build the related types.
-		val element = elements(elementName)
-		(element \ "complexType" \ "sequence" \ "element").map(generateField(_))
+	def handleInputElement(elementName: String) {
+		handleElement(elementName, Direction.Serialize)
 	}
 	
-	def complexType(xmlName: String) : ComplexTypeInfo = {
-		complexTypes.getOrElse(xmlName, makeComplexType(xmlName))
+	def handleOutputElement(elementName: String) {
+		handleElement(elementName, Direction.Deserialize)
+	}
+	
+	private def handleElement(elementName: String, dir: Direction.Value) {
+		// walk through the element decl and build the related types.
+		val element = elements(elementName)
+		(element \ "complexType" \ "sequence" \ "element").map(generateField(_, dir))
+	}
+	
+	def complexType(xmlName: String, dir: Direction.Value) : ComplexTypeInfo = {
+		val t = complexTypes.getOrElse(xmlName, makeComplexType(xmlName, dir))
+		t.direction += dir
+		return t
 	}
 	
 	def writeTypes() {
@@ -92,29 +109,32 @@ class Schema(wsdl: Elem, typeMapping: Map[String, TypeInfo]) {
 		return collection.immutable.Map[String, Node]()
 	}
 	
-	private def makeComplexType(xmlName: String): ComplexTypeInfo = {
+	private def makeComplexType(xmlName: String, dir: Direction.Value): ComplexTypeInfo = {
 		val ct = complexTypeElems(xmlName)
-		val fields = (ct \ "sequence" \ "element").map( x => generateField(x) )
+		// we insert a temporary version of the complexType to handle recursive definitions
+		complexTypes(xmlName) = new ComplexTypeInfo(xmlName, List())
+		val fields = (ct \ "sequence" \ "element").map( x => generateField(x, dir) )
 		val i = new ComplexTypeInfo(xmlName, fields)
+		i.direction += dir
 		complexTypes(xmlName) = i
 		return i
 	}
 
-	private def generateField(field: Node): ComplexTypeProperty = {
+	private def generateField(field: Node, dir: Direction.Value): ComplexTypeProperty = {
 		val max = (field \ "@maxOccurs").text
 		val array = (max != "" && max != "1")
 		val xmlt = elementType(field)
 		val name = (field \ "@name").text
-		val singleType = getType(xmlt)
+		val singleType = getType(xmlt, dir)
 		val t = if (array) new ArrayTypeInfo(singleType) else singleType
 		new ComplexTypeProperty(name, t)
 	}
 
-	private def getType(xmlName: String): TypeInfo = {
+	private def getType(xmlName: String, dir: Direction.Value): TypeInfo = {
 		if (typeMapping contains xmlName) return typeMapping(xmlName)
-		if (complexTypes contains xmlName) return complexTypes(xmlName)
+		if (complexTypes contains xmlName) return complexType(xmlName, dir)
 		if (simpleTypes contains xmlName) return typeMapping("string")	// are all simple types string extentions/restrictions ?
-		makeComplexType(xmlName)	// no where else, assume its a complexType we haven't processed yet
+		makeComplexType(xmlName, dir)	// no where else, assume its a complexType we haven't processed yet
 	}
 	
 	private def elementType(e: Node): String = {
@@ -130,32 +150,30 @@ def stripPrefix(v: String): String = {
 object WSDL2ZKSforce {
 	def main(args: Array[String]) {
 		val types = Map(
-					"string" -> new TypeInfo("string", 	"NSString *", "retain"),
-					"int" 	 -> new TypeInfo("int",    	"NSInteger",  "assign"),
-					"boolean"-> new TypeInfo("boolean", "BOOL", 	  "assign"),
-					"ID"	 -> new TypeInfo("ID",		"NSString *", "retain"),
-					"sObject"-> new TypeInfo("sObject", "ZKSObject *","retain"),
-					"dateTime"-> new TypeInfo("dateTime","NSDate *",  "retain"),
-					"date"   -> new TypeInfo("date",    "NSDate *",   "retain")
+					"string" 		-> new TypeInfo("string", 		"NSString *", 	"retain"),
+					"int" 	 		-> new TypeInfo("int",    		"NSInteger",  	"assign"),
+					"boolean"		-> new TypeInfo("boolean", 		"BOOL", 	  	"assign"),
+					"ID"	 		-> new TypeInfo("ID",			"NSString *", 	"retain"),
+					"sObject"		-> new TypeInfo("sObject", 		"ZKSObject *",	"retain"),
+					"dateTime"		-> new TypeInfo("dateTime",		"NSDate *",  	"retain"),
+					"date"   		-> new TypeInfo("date",    		"NSDate *",   	"retain"),
+					"base64Binary" 	-> new TypeInfo("base64Binary", "NSData *", 	"retain")
 					)
 					
 		val wsdl = XML.loadFile("./partner.wsdl")
 		val schema = new Schema(wsdl, types)
 		for (op <- (wsdl \ "portType" \ "operation")) {
 			val opName = (op \ "@name").text
-			val inMsg = schema.messages(stripPrefix((op \ "input" \ "@message").text))
-			val inElm = stripPrefix((inMsg \ "part" \ "@element").text)
-			println(opName.padTo(40, ' ') + inElm)
-			schema.handleElement(inElm)
+			val inMsg  = schema.messages(stripPrefix((op \ "input" \ "@message").text))
+			val outMsg = schema.messages(stripPrefix((op \ "output" \ "@message").text))
+			val inElm  = stripPrefix((inMsg \ "part" \ "@element").text)
+			val outElm = stripPrefix((outMsg \ "part" \ "@element").text)
+			println(opName.padTo(40, ' ') + inElm.padTo(40, ' ' ) + outElm)
+			schema.handleInputElement(inElm)
+			schema.handleOutputElement(outElm)
 		}
 		
 		schema.writeTypes()
-		
-//		val schemas = (wsdl \ "types" \ "schema" )
-//		for (schema <- schemas)
-//			if ((schema \ "@targetNamespace").text == "urn:partner.soap.sforce.com")
-//				for (ct <- (schema \ "complexType"))
-//					generateComplexType(ct, types).writeHeaderFile()
   	}
 }
 
